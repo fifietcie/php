a95a821d09f8ad91cb6904271c15ecdeeff2f367
Fatma Benalia <benaliafatma@gmail.com>
2023-10-04 10:26:36 +0200

    initialisation php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+include('./header.php'); //permet de linker les autres pages php
+echo "<h1>Ceppic PHP</h1>";   //"echo" = affichage
+?>
+
+<!-- ?> s'utilise seulement pour fermer les lignes de PHP pour les differencier de l'html -->
+
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, laboriosam labore ullam nulla quae aliquam, inventore minus deleniti dolorem vitae eligendi nesciunt, culpa qui assumenda perspiciatis mollitia quas cum enim!</p>
+
+
+<?php
+echo "Encore Ceppic PHP";
+include('./footer.php');
+?>
